0e7962d52778316f588a64cb4bd2fd2eaf876b61
SEED-JYK-LT\admin <likerdo@naver.com>
2019-12-18 09:22:55 +0900

    수정/조회 기능 추가

---
+++ src/main/java/springboot/web/dto/PostUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package springboot.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
