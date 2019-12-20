4c98f780610b4325aab4690c663a2b8dfd90067f
SEED-JYK-LT\admin <likerdo@naver.com>
2019-12-20 16:45:35 +0900

    @LoginUser 생성 및 적용

--- src/main/java/springboot/web/IndexController.java
+++ src/main/java/springboot/web/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import springboot.config.auth.LoginUser;
 import springboot.config.auth.dto.SessionUser;
 import springboot.domain.user.User;
 import springboot.service.PostsService;
@@ -20,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user);
