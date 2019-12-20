47a8d25b9b5be830a2f38b52037e17d2329517e0
SEED-JYK-LT\admin <likerdo@naver.com>
2019-12-20 13:00:33 +0900

    oAuth2 구글 로그인 연동 기능 추가

--- src/main/java/springboot/web/IndexController.java
+++ src/main/java/springboot/web/IndexController.java
@@ -5,29 +5,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import springboot.config.auth.dto.SessionUser;
+import springboot.domain.user.User;
 import springboot.service.PostsService;
 import springboot.web.dto.PostResponseDto;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
+        if (user != null) {
+            model.addAttribute("userName", user);
+        }
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
+    public String postsUpdate(@PathVariable Long id, Model model) {
         PostResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
         return "posts-update";
