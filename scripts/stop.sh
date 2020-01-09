c9603c8cd3fea38a572e53728b82a691582a2bb9
SEED-JYK-LT\admin <likerdo@naver.com>
2020-01-09 13:05:06 +0900

    무중단 배포 테스트 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,16 +1,18 @@
-ABSDIR=$(dirname $ABSPATH)
+#!/usr/bin/env bash
 
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-IDLE_PORT=$(find_idel_port)
+IDLE_PORT=$(find_idle_port)
 
-echo "> $IDEL_PORT 에서 구동 중인 애플리케이션의 PID를 확인하는 중입니다."
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
-if [ -z ${IDLE_PORT} ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z ${IDLE_PID} ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> 현재 구동 중인 애플리메이션($IDLE_PID)을 종료합니다."
+  echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
   sleep 5
 fi
